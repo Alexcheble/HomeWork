ee6870654e1bbd02013e087087c8e3ab94d5a87d
Alexushka <Alexushkasppmister3@gmail.com>
2018-11-30 01:33:50 +0200

    less5hm4

--- Lesson 5 homework/4/main.js
+++ Lesson 5 homework/4/main.js
@@ -1,12 +1,18 @@
 holst.addEventListener('click', function(){
 	var block = document.createElement('div');
 	for (var i = 0; i < document.querySelectorAll('.figura-element').length; i++) { 
-		if (document.querySelectorAll('.figura-element')[i].checked === true ) {
+		if (document.querySelectorAll('.figura-element')[i].checked === true) {
 		var figure = document.querySelectorAll('.figura-element')[i].value;
 		break
 		}
 	}
-	block.classList.add('block', figure);
+	for (var j = 0; j < document.querySelectorAll('.figura-color').length; j++) {
+		if (document.querySelectorAll('.figura-color')[j].checked === true) {
+			var figureC = document.querySelectorAll('.figura-color')[j].value;
+			break
+		}
+	}
+	block.classList.add(figure,figureC);
 	this.appendChild(block);
 	block.style.left = event.clientX + -20 +'px';
 	block.style.top = event.clientY + -20 +'px';
