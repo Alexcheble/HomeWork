3226bc132b853fa0077fa27e27dce25d6725884e
Alexushka <Alexushkasppmister3@gmail.com>
2018-12-04 14:48:30 +0200

    correct7

--- Lesson 7 homework/function/main.js
+++ Lesson 7 homework/function/main.js
@@ -1,3 +1,9 @@
+// В первом варианте мы удаляем 
 var man = ['Jhon', 'Bob', 'Bill'];
 function remove (arr, ind) {delete arr [ind]; return arr;}
 console.log(remove(man,1));
+// А во втором мы заменяем елемент массива на undefined и этот елемент существует еще в массиве
+var man2 = ['Kurt', 'Michael', 'Orlando'];
+function rem (arr, ind) {arr[ind]=undefined; return arr;}
+console.log(rem(man2,1));
+
